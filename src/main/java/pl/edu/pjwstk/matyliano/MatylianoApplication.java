c6d40c68985bb12a731cc60334c440cdc8745b78
Matyliano <joanna.boszko@gmail.com>
2021-05-16 13:26:37 +0200

    fisrt commit

---
+++ src/main/java/pl/edu/pjwstk/matyliano/MatylianoApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pjwstk.matyliano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatylianoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatylianoApplication.class, args);
+    }
+
+}
